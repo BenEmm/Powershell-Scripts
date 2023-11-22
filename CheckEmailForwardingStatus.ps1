ef9d2c1e1f542914b900db7bcad422a033d03319
BenEmm <78164631+BenEmm@users.noreply.github.com>
2023-11-22 23:32:54 +0000

    Added two scripts

---
+++ CheckEmailForwardingStatus.ps1
@@ -0,0 +1,48 @@
+﻿# List of users to check
+$users = @(
+    # Insert users names here in the format "Jeff Jefferson",
+)
+
+# Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName #Your email here
+
+# Function to check mail forwardinga
+function Check-MailForwarding {
+    param ($user)
+    $mailbox = Get-Mailbox -Identity $user -ErrorAction SilentlyContinue
+    if ($mailbox) {
+        $forwardingStatus = ""
+        $forwardingAddress = $mailbox.ForwardingAddress
+        $forwardingSmtpAddress = $mailbox.ForwardingSmtpAddress
+
+        if ($forwardingAddress) {
+            $forwardingStatus = "Mail is being forwarded to an internal address."
+        } elseif ($forwardingSmtpAddress) {
+            $forwardingStatus = "Mail is being forwarded to $($forwardingSmtpAddress)."
+        }
+
+        # Check for forwarding set via inbox rules
+        $inboxRules = Get-InboxRule -Mailbox $user -ErrorAction SilentlyContinue
+        foreach ($rule in $inboxRules) {
+            if ($rule.ForwardTo -or $rule.ForwardAsAttachmentTo) {
+                $forwardingStatus += " Mail forwarding rule found: $($rule.Name)."
+            }
+        }
+
+        if ($forwardingStatus -eq "") {
+            "$user's mail is not being forwarded."
+        } else {
+            "$user - $forwardingStatus"
+        }
+    } else {
+        "Mailbox for $user not found."
+    }
+}
+
+# Check each user
+foreach ($user in $users) {
+    Check-MailForwarding -user $user
+}
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false
---
+++ CreateMultipleDistributionListsFromFile.ps1
@@ -0,0 +1,38 @@
+﻿# Connect to Exchange Online
+# You will need to login with an account that has the necessary permissions
+Connect-ExchangeOnline
+
+# Import email addresses from a file
+$emailList = Get-Content "C:\File\Path\Here\FileName.txt"
+
+# Loop through each email address
+foreach ($email in $emailList) {
+    try {
+        # Create the Distribution Group
+        $group = New-DistributionGroup -Name $email -PrimarySmtpAddress $email -ErrorAction Stop
+        Write-Host "Distribution Group created for $email"
+
+        # Add whoever you need as a member
+        # For multiple people, better to put them all in a distribution group then add that one group here
+        Add-DistributionGroupMember -Identity $email -Member "Person@domain.com" -ErrorAction Stop
+        Write-Host "Person@domain.com added to $email"
+
+        # Set Delivery Management settings
+        Set-DistributionGroup -Identity $email -RequireSenderAuthenticationEnabled $false
+        Write-Host "Delivery management settings updated for $email"
+
+        # Set Message Approval settings
+        Set-DistributionGroup -Identity $email -ModerationEnabled $false
+        Write-Host "Message approval settings updated for $email"
+
+        # Set Membership Approval settings
+        Set-DistributionGroup -Identity $email -MemberJoinRestriction Closed -MemberDepartRestriction Open
+        Write-Host "Membership approvals settings updated for $email"
+
+    } catch {
+        Write-Host "Error processing ${email}: $_"
+    }
+}
+
+# Disconnect from Exchange Online
+# Disconnect-ExchangeOnline
